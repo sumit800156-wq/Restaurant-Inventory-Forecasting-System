6933671433213129b95664b5467a0b53cc28eb58
sumit800156-wq <sumit800156@gmail.com>
2026-06-14 22:12:26 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,17 +1,24 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
 import pickle
 
 # Dataset Load
 data = pd.read_csv("dataset.csv")
 
 # Input aur Output
-X = data[["Day"]]
+X = data[["Day", "Temperature", "Weekend"]]
 y = data["Sales"]
 
 # Model Training
 model = LinearRegression()
 model.fit(X, y)
+
+# Accuracy Check
+pred = model.predict(X)
+score = r2_score(y, pred)
+
+print("Accuracy Score:", round(score, 2))
 
 # Save Model
 pickle.dump(model, open("model.pkl", "wb"))
